687485a8bfb58d561ec776042ed25221b0061529
fautea <lascauxstephane@gmail.com>
2024-06-17 17:00:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ from sklearn.impute import SimpleImputer
 bank_cleaned = bank.drop(bank.loc[bank["job"] == "unknown"].index, inplace=True)
 bank_cleaned = bank.drop(bank.loc[bank["education"] == "unknown"].index, inplace=True)
 bank_cleaned = bank.drop(['contact', 'pdays'], axis = 1)
-st.write("Le modèle choisi sera la classification en raison des valeurs discrètes de la variable cible 'deposit')
+st.write('Le modèle choisi sera la classification en raison des valeurs discrètes de la variable cible 'deposit')
 bank[deposit].head()
 feats = bank_cleaned.drop(['deposit'], axis = 1)
 target = bank_cleaned['deposit']
